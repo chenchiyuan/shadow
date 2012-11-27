9218373dd102e18eb37b6e5cca39685611ad501b
chenchiyuan <chenchiyuan03@gmail.com>
2012-11-27 23:55:03 +0800

    add func section chain decorate

---
+++ shadow/utils/decorate.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# __author__ = chenchiyuan
+
+from __future__ import division, unicode_literals, print_function
+
+def func_section(before_chain=[], after_chain=[]):
+  def func_wrap(func):
+    def wrap(*args, **kwargs):
+      for before_func in before_chain:
+        try:
+          before_func(*args, **kwargs)
+        except Exception:
+          continue
+      res = func(*args, **kwargs)
+
+      for after_func in after_chain:
+        try:
+          after_func(*args, **kwargs)
+        except Exception:
+          continue
+      return res
+    return wrap
+  return func_wrap
+
+def example():
+  def before_saying(self):
+    print("I know your name %s" % self.name)
+
+  def before_saving(self):
+    print("I will notify your name %s" % self.name)
+
+  def after_saving(self):
+    print("After saving your name %s " % self.name)
+
+  def done(self):
+    print("Everything is ok %s " % self.name)
+
+  class FuncTest():
+    def __init__(self, name):
+      self.name = name
+
+    @func_section(before_chain=[before_saying, before_saving],
+      after_chain=[after_saving, done])
+    def save(self):
+      print("Save name %s " % self.name)
+
+  test = FuncTest(name=u'chenchiyuan')
+  test.save()
